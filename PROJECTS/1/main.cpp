af3abdd227e0ae7f5a6b944fd8bb19575ea9206f
yxj <564197835@qq.com>
2022-06-04 02:10:39 +0800

    perfect Q1

--- PROJECTS/1/main.cpp
+++ PROJECTS/1/main.cpp
@@ -1,25 +1,40 @@
 // https://leetcode.cn/problems/two-sum/
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值
+// target  的那两个整数，并返回它们的数组下标。
 
 #include <cstdio>
+#include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
-  void twoSum(std::vector<int>& nums, int target) {
-    for (int i = 0; i <= nums.size(); i++) {
-      printf("%d ", i);
+  std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    for (unsigned long i = 0; i < nums.size(); i++) {
+      for (unsigned long j = i + 1; j < nums.size(); j++) {
+        if (nums[i] + nums[j] == target) {
+          std::vector<int> result;
+          result.push_back(i);
+          result.push_back(j);
+          return result;
+        }
+      }
     }
-    printf("\n");
+    printf("[ERROR] unreachable\n");
+    abort();
   }
 };
 
 int main() {
-  printf("hello\n");
-  printf("hello\n");
-  std::vector<int> a;
-  for (int i = 0; i < 5; i++) {
-    a.push_back(i);
-  }
+  printf("[TRACE] START\n");
 
-  Solution().twoSum(a, 5);
+  std::vector<int> nums = {3, 2, 4};
+  int target = 6;
+
+  std::vector<int> result = Solution().twoSum(nums, target);
+  for (unsigned long i = 0; i < result.size(); i++) {
+    printf("%d ", result[i]);
+  }
+  printf("\n");
+
+  printf("[TRACE] END\n");
 }
